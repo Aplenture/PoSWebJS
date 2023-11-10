b9975d5f869dd71a0bc9d84c39cbc484643b0974
fweise <public@weise-games.de>
2023-11-10 13:37:55 +0100

    clean up

--- src/enums/balanceEvent.ts
+++ src/enums/balanceEvent.ts
@@ -1,3 +1,10 @@
+/**
+ * Aplenture/PoSWebJS
+ * https://github.com/Aplenture/PoSWebJS
+ * Copyright (c) 2023 Aplenture
+ * License https://github.com/Aplenture/PoSWebJS/blob/main/LICENSE
+ */
+
 export enum BalanceEvent {
     Deposit = "deposit",
     Invoice = "invoice",
--- src/enums/balanceResolution.ts
+++ src/enums/balanceResolution.ts
@@ -1,8 +1,8 @@
 /**
- * Aplenture/BackendJS
- * https://github.com/Aplenture/BackendJS
+ * Aplenture/PoSWebJS
+ * https://github.com/Aplenture/PoSWebJS
  * Copyright (c) 2023 Aplenture
- * MIT License https://github.com/Aplenture/BackendJS/blob/main/LICENSE
+ * License https://github.com/Aplenture/PoSWebJS/blob/main/LICENSE
  */
 
 export enum BalanceResolution {
